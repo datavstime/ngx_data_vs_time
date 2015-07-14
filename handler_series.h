cad8fa572b04cdf6eddf71d05f270b6963c0ed1a
Matt Howlett <matt@mhowlett.com>
2015-07-14 14:30:28 +0700

    capitalized sin series labels

--- handler_series.h
+++ handler_series.h
@@ -8,7 +8,7 @@ static ngx_str_t handle_series(ngx_http_request_t *r)
   ngx_str_t result_body;
 
   result_body.data = ngx_pcalloc(r->pool, 256);
-  strcpy(result_body.data, "['sin4', 'sin5', 'sin6', 'sin7', 'sin8', 'sin9', 'sin10']");
+  strcpy(result_body.data, "['SIN4', 'SIN5', 'SIN6', 'SIN7', 'SIN8', 'SIN9', 'SIN10']");
   result_body.len = strlen(result_body.data);
 
   return result_body;
