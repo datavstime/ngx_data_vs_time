c11b93e9344728b16ad00ffe6a548b829d0550ea
Matt Howlett <matt@mhowlett.com>
2015-07-14 14:52:48 +0700

    formatting changes to series

--- handler_series.h
+++ handler_series.h
@@ -8,7 +8,7 @@ static ngx_str_t handle_series(ngx_http_request_t *r)
   ngx_str_t result_body;
 
   result_body.data = ngx_pcalloc(r->pool, 256);
-  strcpy(result_body.data, "['SIN4', 'SIN5', 'SIN6', 'SIN7', 'SIN8', 'SIN9', 'SIN10']");
+  strcpy(result_body.data, "[\"SIN4\",\"SIN5\",\"SIN6\",\"SIN7\",\"SIN8\",\"SIN9\",\"SIN10\"]");
   result_body.len = strlen(result_body.data);
 
   return result_body;
