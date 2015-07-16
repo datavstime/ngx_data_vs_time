ae6adefc898823e13d646736e7ff7eca1a58acdb
Matt Howlett <matt@mhowlett.com>
2015-07-16 20:34:22 +0700

    trying murmur3 hash fn

--- ngx_http_data_vs_time_module.c
+++ ngx_http_data_vs_time_module.c
@@ -15,11 +15,90 @@ typedef struct functionObject_t
   double (*fn)(struct functionObject_t* d, int64_t t);
 } functionObject_t;
 
-// really fast and good enough for current purposes.
-static uint32_t randomint(uint32_t seed)
+
+// START Murmur3
+
+#define FORCE_INLINE __attribute__((always_inline))
+
+FORCE_INLINE uint32_t fmix ( uint32_t h )
 {
-  return seed * (uint32_t)2654435761;
+  h ^= h >> 16;
+  h *= 0x85ebca6b;
+  h ^= h >> 13;
+  h *= 0xc2b2ae35;
+  h ^= h >> 16;
+
+  return h;
 }
+
+FORCE_INLINE uint32_t getblock ( const uint32_t * p, int i )
+{
+  return p[i];
+}
+
+inline uint32_t rotl32 ( uint32_t x, int8_t r )
+{
+  return (x << r) | (x >> (32 - r));
+}
+#define ROTL32(x,y)     rotl32(x,y)
+
+void MurmurHash3_x86_32 ( const void * key, int len,
+                          uint32_t seed, void * out )
+{
+  const uint8_t * data = (const uint8_t*)key;
+  const int nblocks = len / 4;
+
+  uint32_t h1 = seed;
+
+  uint32_t c1 = 0xcc9e2d51;
+  uint32_t c2 = 0x1b873593;
+
+  //----------
+  // body
+
+  const uint32_t * blocks = (const uint32_t *)(data + nblocks*4);
+
+  int i;
+  for(i = -nblocks; i; i++)
+  {
+    uint32_t k1 = getblock(blocks,i);
+
+    k1 *= c1;
+    k1 = ROTL32(k1,15);
+    k1 *= c2;
+
+    h1 ^= k1;
+    h1 = ROTL32(h1,13);
+    h1 = h1*5+0xe6546b64;
+  }
+
+  //----------
+  // tail
+
+  const uint8_t * tail = (const uint8_t*)(data + nblocks*4);
+
+  uint32_t k1 = 0;
+
+  switch(len & 3)
+  {
+  case 3: k1 ^= tail[2] << 16;
+  case 2: k1 ^= tail[1] << 8;
+  case 1: k1 ^= tail[0];
+          k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+  };
+
+  //----------
+  // finalization
+
+  h1 ^= len;
+
+  h1 = fmix(h1);
+
+  *(uint32_t*)out = h1;
+}
+
+// --- END Murmur3
+
 
 static double pingValueCreator(functionObject_t* fo, int64_t t)
 {
@@ -62,7 +141,11 @@ static double mixValueCreator(functionObject_t* fo, int64_t t)
 static double wtValueCreator(functionObject_t* fo, int64_t t)
 {
   uint32_t multiplier = ((uint32_t *)(fo->data))[0];
-  return ((double)randomint((uint32_t)t) / (double)UINT_MAX) * multiplier;
+
+  uint32_t out;
+  MurmurHash3_x86_32 ( &t, sizeof(int64_t), 0, &out);
+
+  return ((double)out / (double)UINT_MAX) * multiplier;
 }
 
 static ngx_str_t values_handler(ngx_http_request_t *r)
