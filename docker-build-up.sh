d1b543fa7d612a263d6b0c99bd6a2a5d8f4720df
Matt Howlett <matt@mhowlett.com>
2015-08-09 11:04:46 +0700

    sdf

--- docker-build-up.sh
+++ docker-build-up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -it -p 8000:8000 \
+docker run --rm -it -p 8000:80 \
   -v /git/ngx_data_vs_time/:/repo \
   -v /git/ngx_data_vs_time/:/var/www \
   mhowlett/nginx-build-base /bin/bash
